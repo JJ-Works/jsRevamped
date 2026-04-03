d6daa32fbd8ea625c01f4fb06a0a282f4ee60d56
Janarjan <janarjanrb@gmail.com>
2026-04-03 17:15:16 +0545

    fianled

---
+++ functions.js
@@ -0,0 +1,12 @@
+// lets look into functions
+
+let printf = function print(x){
+    console.log(`${x}`)
+}
+
+// printf("hello world")
+
+
+const funcOne = () => (console.log("FuncOne says hello world");)
+
+funcOne()
